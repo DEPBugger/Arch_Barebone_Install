c5c32db7aebe653a1e735e2548c275157db2221b
DEPBugger <kegxsws@hi2.in>
2017-05-24 14:43:16 +0200

    Im still working on the scripts...

--- Test2.bash
+++ Test2.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 systemctl enable NetworkManager.service && systemctl enable lxdm.service && systemctl start NetworkManager.service && systemctl start lxdm.service
-# sed o rm para autoeliminar el lanzador de este script en el rc local
+# sed o rm para autoeliminar el lanzador de este script
 rm $0
