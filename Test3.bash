a7d24b95daffbe5aa320749fb9437da6de23296f
DEPBugger <kegxsws@hi2.in>
2017-05-24 20:36:24 +0200

    Deleted Test3.bash

--- Test3.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-systemctl enable NetworkManager.service && systemctl enable lxdm.service && systemctl start NetworkManager.service && systemctl start lxdm.service
-# sed o rm para eliminar el lanzador de este script
-rm $0
