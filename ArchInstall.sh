941fe3a96e6526da50480579ce792915c8ef6eeb
fryntiz <tecnico@fryntiz.es>
2017-05-25 15:10:27 +0200

    Añadidos comentarios de posibles funciones

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -22,9 +22,13 @@ function preconfig() {
 	loadkeys es 2>> $LOG && echo "Teclado del entorno live configurado a español" >> $LOG #Establece teclado en español
 
 	mount -o remount,size=2G /run/archiso/cowspace 2>>$LOG && echo "/run/archiso/cowspace ampliada a 2GB" >> $LOG
+
+	#Comprobar internet
+
+	#Actualizar pacman y git sin pedir confirmación?=?
+	# pacman -Syy --noconfirm git
+	# echo "git instalado"
 }
-
-
 
 #LLamada a las funciones
 preconfig
