b9c9e24b7165138e6de813ca936e4e699785dfa2
fryntiz <tecnico@fryntiz.es>
2017-05-25 17:25:08 +0200

    Reañadidas funciones

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -57,8 +57,80 @@ function preconfig() {
 	# echo "git instalado"
 }
 
+function ConfDisk() {
+	# Mostrar discos y particiones actuales
+	# Planetear si esto es interactivo por ejemplo:
+	# Tienes X discos duros elige donde instalar (si tiene solo 1 continuar)
+	# Plantear un sistema para elegir cantidad de particiones y si separar /boot /home y /
+	echo "Comprobando tus Discos Duros y Particiones en ellos."
+	sleep 1s
+	echo "Comprobando tus Discos Duros y Particiones en ellos.."
+	sleep 1s
+	echo "Comprobando tus Discos Duros y Particiones en ellos..."
+	sleep 1s
+	lsblk
+
+	#Pedir introducir disco o crear bucle con cada uno de ellos para elegirlo
+
+	echo "El disco elegido es: XXXX???"
+	echo ""
+	echo "Ahora deberás editar el particionado de tu disco"
+	echo "Pulsa una tecla para continuar..."
+	read input #Se añade esta entrada para hacer pausa y que el usuario vea los discos
+
+	# Añadir opción para eliminar la tabla de particiones
+	cfdisk /dev/sda
+	lsblk
+	# ¿Es correcto?
+	# Añadir opción para editar el /dev/sdXY de los mkfs
+	# mkfs.vfat -F32 /dev/sda1
+	# mkfs.ext4 -L "Arch Linux" /dev/sda2
+	# mkfs.ext4 -L "home" /dev/sda3
+	# mkswap /dev/sda4
+	# swapon /dev/sda4
+	# mkdir /mnt/home
+	# mount /dev/sda2 /mnt
+	# mkdir -p /mnt/boot/efi
+	# mount /dev/sda1 /mnt/boot/efi
+	lsblk
+	# ¿Está correcto?
+	# mount /dev/sda3 /mnt/home
+}
+
+function PreInstall() {
+	echo '########---Instalando reflector y actualizando la mirrorlist del live---########'
+	pacman -Syy reflector --noconfirm
+	reflector --latest 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+	pacstrap /mnt base base-devel networkmanager net-tools
+	genfstab -U -p /mnt >> /mnt/etc/fstab
+
+
+	#arch-chroot /mnt bash Test2.bash
+}
+
+function ToInstall() {
+	#Comprobar que está bien montado
+
+	#Instalar
+	#cp ~/Arch_Barebone_Install/Test2.bash /mnt
+
+	#Comprobar que la copia se ha realizado correctamente
+	ls /mnt/tmp
+}
+
+function PostInstall() {
+	echo "Se ha terminado de instalar, ¿desea apagar el equipo?"
+	#Preguntar si o no [s/n]
+	#En caso de si apagar:
+	#poweroff
+}
+
 #LLamada a las funciones
 InfoHelp
 preconfig
+ConfRed
+ConfDisk
+PreInstall
 
 exit 0
+
