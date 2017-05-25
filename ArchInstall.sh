d406eb8dceba9b750f8a831ec23007eac12ed16b
fryntiz <tecnico@fryntiz.es>
2017-05-25 14:55:17 +0200

    Comprobar escritura en el archivo LOG

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -5,10 +5,14 @@ USER=`whoami`
 DEBUG="true"
 LOGDIR="/tmp/ArchInstall"
 
-
-#Comprobar root
-
 function preconfig() {
 	touch $LOGDIR #Crea el archivo donde se registrará el LOG
+	if [ ! -w $LOGDIR ]; then #Comprobar que es posible escribir en el archivo log
+		echo "Error al crear el archivo temporal"
+		echo "Saliendo del instalador"
+		exit 1
+	fi
 	loadkeys es && echo "Teclado del entorno live configurado a español" >> $LOGDIR #Establece teclado en español
 }
+
+exit 0
