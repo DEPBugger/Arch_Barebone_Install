feb3f34a230a419f690fe6cf81f643c10051fd7a
DEPBugger <kegxsws@hi2.in>
2017-05-26 15:35:34 +0200

    One step closer...

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -6,7 +6,7 @@ USER=`whoami`
 DEBUG="true"
 LOG="/tmp/ArchInstall"
 
-#TERMINADO (Solo falta rellenar información en los "echo" con advertencias de que algo puede fallar)
+#TERMINADO y traducido (Solo falta rellenar información en los "echo" con advertencias de que algo puede fallar)
 #Function that gives information about the installation process
 function InfoHelp() {
 	clear
@@ -28,7 +28,7 @@ function InfoHelp() {
 			case $input in
 				Y|y) break;;
 				N|n) exit 0;;
-				*) echo "Invalid answer, please just answer y for yes or n for no [y/n]";;
+				*) echo "Invalid answer, please answer just y for yes or n for no [y/n]";;
 			esac
 		done
 }
@@ -48,24 +48,29 @@ function PreConfig() {
 	function selectKeyboard() {
 		while true; do
 			clear
-			echo "Introduce el idioma de teclado"
+			echo "Select your keyboard layout"
 			# https://wiki.archlinux.org/index.php/Keyboard_configuration_in_console
-			echo "es Para Español"
-			echo "uk Para Inglés Inglaterra"
-			echo "fr Para Francés"
-			echo "de Para Alemán"
-			echo "us Para Inglés de USA"
+			echo "es for spanish keyboard"
+			echo "uk for United Kingdom english layout"
+			echo "fr for frech layout"
+			echo "de for deutsch layout"
+			echo "us for United States of America english layout"
 
 			read LANGUAGE
 
 			case $LANGUAGE in
-				es|Es|ES) break;;
-				uk|Uk|UK) break;;
-				fr|Fr|FR) break;;
-				de|De|DE) break;;
-				us|Us|US) break;;
-				*) echo "Opción no válida, inserta solo una de las opciones anteriores"
-					echo "Pulsa intro para continuar"
+				es|ES) LANGUAGE=es
+						break;;
+				uk|UK) LANGUAGE=uk
+						break;;
+				fr|FR) LANGUAGE=fr
+						break;;
+				de|DE) LANGUAGE=de
+						break;;
+				us|US) LANGUAGE=us
+						break;;
+				*) echo "Invalid choice, write just es, uk, fr, de or us"
+					echo "Press Enter to continue"
 					read input;;
 			esac
 		done
@@ -73,29 +78,29 @@ function PreConfig() {
 		# https://wiki.archlinux.org/index.php/Keyboard_configuration_in_console#Keymap_codes
 		##### Filtrar códigos disponibles para loadkeys --> `find /usr/share/kbd/keymaps/ -type f`
 		# Establece teclado en el idioma elegido
-		loadkeys $LANGUAGE 2>> $LOG && echo "Teclado del entorno live configurado a $LANGUAGE" >> $LOG
+		loadkeys $LANGUAGE 2>> $LOG && echo "Live environment keyboard layout has been set to $LANGUAGE" >> $LOG
 		echo ""
-		echo "El idioma de teclado actual es el siguiente:"
-		localectl status #Muestra distribución actual del teclado
+		echo "Keyboard layout actual info:"
+		localectl status #Shows keyboard layout actual info
 		echo " "
 	}
 
 	selectKeyboard
 
 	while true; do
-		echo "Es correcta la distribución [y/n]"
+		echo "It is right? [y/n]"
 
 		read input
 
 		case $input in
 			y|Y) break;;
 			n|N) selectKeyboard;;
-			*) echo "Opción no válida"
+			*) echo "Invalid answer, please answer just y for yes or n for no [y/n]"
 		esac
 	done
 
-	mount -o remount,size=2G /run/archiso/cowspace 2>> $LOG && echo "/run/archiso/cowspace ampliada a 2GB" >> $LOG
-	echo "/run/archiso/cowspace ampliada a 2GB"
+	mount -o remount,size=2G /run/archiso/cowspace 2>> $LOG && echo "Live system memory (/run/archiso/cowspace) set to 2GB" >> $LOG
+	echo "Live system memory (/run/archiso/cowspace) set to 2GB"
 }
 
 function ConfRed() {
