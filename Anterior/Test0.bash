bfc32f0e6be701611825917239757b05e891dfd9
fryntiz <tecnico@fryntiz.es>
2017-05-28 16:44:14 +0200

    Reestructurando

--- Anterior/Test0.bash
+++ Anterior/Test0.bash
@@ -12,21 +12,21 @@ echo '
                    -`                     Welcome to an awesome Arch install script
                   .o+`                    This script will install Arch Linux in a
                  `ooo/                    very comfortable way for you.
-                `+oooo:                   
+                `+oooo:
                `+oooooo:                  Please have a seat and enjoy seeing lot of
                -+oooooo+:                 text lines appearing on your screen and
              `/:-:++oooo+:                write your password or say yes or no when
             `/++++/+++++++:               required.
-           `/++++++++++++++:              
-          `/+++ooooooooooooo/`            
-         ./ooosssso++osssssso+`           
-        .oossssso-````/ossssss+`          
-       -osssssso.      :ssssssso.         
-      :osssssss/        osssso+++.        
-     /ossssssss/        +ssssooo/-        
-   `/ossssso+/:-        -:/+osssso+-      
-  `+sso+:-`                 `.-/+oso:     
- `++:.                           `-/+/    
+           `/++++++++++++++:
+          `/+++ooooooooooooo/`
+         ./ooosssso++osssssso+`
+        .oossssso-````/ossssss+`
+       -osssssso.      :ssssssso.
+      :osssssss/        osssso+++.
+     /ossssssss/        +ssssooo/-
+   `/ossssso+/:-        -:/+osssso+-
+  `+sso+:-`                 `.-/+oso:
+ `++:.                           `-/+/
  .`                                 `/    '
 read -p 'Press Enter to begin installation process.'
 mount -o remount,size=2G /run/archiso/cowspace
---
+++ InfoHelp.sh
@@ -0,0 +1,27 @@
+#TERMINADO y traducido (Solo falta rellenar información en los "echo" con advertencias de que algo puede fallar)
+#Function that gives information about the installation process
+function InfoHelp() {
+	clear
+	echo "This installer script will takes the following steps, please write them down so that you can easily follow the installation process"
+	echo "1) ....."
+	echo "2) ....."
+	echo "3) ....."
+	echo "4) ....."
+	echo "5) ....."
+	echo "6) ....."
+	echo ""
+	echo "From now on keep in mind that when you see [y/n] you must answer y (affirmative answer) or n (negative answer)"
+	echo "Continue? [y/n]"
+
+	while true
+		do
+			read input
+
+			case $input in
+				Y|y) break;;
+				N|n) exit 0;;
+				*) echo "Invalid answer, please answer just y for yes or n for no [y/n]";;
+			esac
+		done
+}
+
