650df5bfe442e6724369f94b5b40b98d19ba514c
fryntiz <tecnico@fryntiz.es>
2017-05-28 21:10:57 +0200

    Correcciones

--- confred.sh
+++ confred.sh
@@ -2,9 +2,9 @@
 function ConfRed() {
 	clear
 
-	PIN1=""
-	PIN2=""
-	PIN3=""
+	PING1=""
+	PING2=""
+	PING3=""
 	CORRECT_RED="false"
 
 	echo "Activando Cliente DHCP" && echo "Activando Cliente DHCP" >> $LOG
@@ -14,9 +14,9 @@ function ConfRed() {
 	# Función para comprobar la red
 	function correctRed() {
 
-		PIN1="`ping -c1 www.archlinux.org 2>> /dev/null >> /dev/null && echo true || echo false`"
-		PIN2="`ping -c1 www.archlinux.org 2>> /dev/null >> /dev/null && echo true || echo false`"
-		PIN3="`ping -c1 www.archlinux.org 2>> /dev/null >> /dev/null && echo true || echo false`"
+		PING1="`ping -c1 www.archlinux.org 2>> /dev/null >> /dev/null && echo true || echo false`"
+		PING2="`ping -c1 www.archlinux.org 2>> /dev/null >> /dev/null && echo true || echo false`"
+		PING3="`ping -c1 www.archlinux.org 2>> /dev/null >> /dev/null && echo true || echo false`"
 
 		if $PING1 || $PING2 || $PING3; then
 			echo "Hay conexión a internet" && echo "Conexión a internet correcta" >> $LOG
@@ -30,6 +30,7 @@ function ConfRed() {
 	function configRed() {
 		echo "Intentando configurar la red"
 		# Intentar conectar por cable, si falla intentar conectar por wireless
+		dhcpcd 2>> $LOG && echo "El comando dhcpcd ha funcionado correctamente" >> $LOG
 	}
 
 	# Comprobar si la red es correcta o volver a intentar
@@ -38,11 +39,4 @@ function ConfRed() {
 	else
 		correctRed
 	fi
-
-
-
-	dhcpcd 2>> $LOG && echo "El comando dhcpcd ha funcionado correctamente" >> $LOG
-
-	# Comprobar conexión a internet, mirar que en 1 de 3 ping haya obtenido respuesta de www.kernel.org
-	echo "Hay conexión de red" >> $LOG
 }
