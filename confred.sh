9797f5c44404c2dd28ddc0b25a8256315fe95348
fryntiz <tecnico@fryntiz.es>
2017-05-28 20:28:29 +0200

    Planteada estructura para controlar la red

--- confred.sh
+++ confred.sh
@@ -1,9 +1,32 @@
 
 function ConfRed() {
 	clear
+
+	PIN1=""
+	PIN2=""
+	PIN3=""
+	CORRECT_RED="false"
+
 	echo "Activando Cliente DHCP" && echo "Activando Cliente DHCP" >> $LOG
+
+	echo "Para evitar problemas se recomienda el uso de una conexión cableada hacia internet"
+
+	# Función para configurar la red
+	function correctRed() {
+
+	}
+
+	# Comprobar si la red es correcta
+	if $CORRECT_RED; then
+		break;
+	else
+		correctRed
+	fi
+
+
+
 	dhcpcd 2>> $LOG && echo "El comando dhcpcd ha funcionado correctamente" >> $LOG
 
 	# Comprobar conexión a internet, mirar que en 1 de 3 ping haya obtenido respuesta de www.kernel.org
-	# echo "Hay conexión de red" >> $LOG
+	echo "Hay conexión de red" >> $LOG
 }
