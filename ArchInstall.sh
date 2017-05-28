bd99ba10aafe16d48a9671766ddbae1bcd078fb0
fryntiz <tecnico@fryntiz.es>
2017-05-28 15:25:33 +0200

    Estructura de control para la post instalación

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -281,10 +281,19 @@ function ToInstall() {
 
 function PostInstall() {
 	##### Elegir la distribución de teclado que tendrá el sistema instalado, en concreto cuando hay que editar el archivo /etc/vconsole.conf (simplemente añadir KEYMAP=X, de nuevo la X será el código de teclado a usar, busca `vconsole` en Test2.bash para ver como va), la idea es preguntar si desea meter la misma distribución de teclado en el sistema instalado que la que haya elegido en el live.
-	echo "Se ha terminado de instalar, ¿desea apagar el equipo?"
-	#Preguntar si o no [s/n]
-	#En caso de si apagar:
-	#poweroff
+	while true; do
+		clear
+		echo "Se ha terminado de instalar, ¿desea apagar el equipo?"
+		echo ""
+
+		read input
+
+		case $input in
+			s|S) poweroff; break;;
+			n|N) exit 0;;
+			*) echo "Opción inválida [s/n]";;
+		esac
+	done
 }
 
 #LLamada a las funciones, si DEBUG=true se habilita el modo pruebas
