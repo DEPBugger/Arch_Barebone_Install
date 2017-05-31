20199faf718de5b11b94820fb7cf4c6044c8f0aa
fryntiz <tecnico@fryntiz.es>
2017-05-31 15:26:09 +0200

    Planteada instalación de escritorios en la postinstalación

--- postinstall.sh
+++ postinstall.sh
@@ -1,22 +1,46 @@
 
 function PostInstall() {
-	##### Elegir la distribución de teclado que tendrá el sistema instalado, en concreto cuando hay que editar el archivo /etc/vconsole.conf (simplemente añadir KEYMAP=X, de nuevo la X será el código de teclado a usar, busca `vconsole` en Test2.bash para ver como va), la idea es preguntar si desea meter la misma distribución de teclado en el sistema instalado que la que haya elegido en el live.
+	echo "Instalando reflector y actualizando la mirrorlist del live para usar lor mirrors más rápidos"
+	pacman -Syy reflector --noconfirm && echo "reflector instalado correctamente" >> $LOG || echo "reflector no instalado" >> $LOG
+	reflector --latest 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist && echo "Mirror más rápido elegido" >> $LOG
+
+	function MyDesktop() {
+		case $gui in
+			xfce|Xfce|XFCE)
+				echo "Instalando XFCE"
+				#xfce4 y xfce4-goodies
+			;;
+			gnome|Gnome|GNOME)
+				echo "Instalando GNOME SHELL"
+			;;
+			plasma|Plasma|PLASMA)
+				echo "Instalando PLASMA"
+			;;
+			cinnamon|Cinnamon|CINNAMON)
+				echo "Instalando CINNAMON"
+			;;
+			mate|Mate|MATE)
+				echo "Instalando MATE"
+				#mate y mate-extra
+			;;
+			*)
+				echo "No se instalará escritorio ya que no se ha elegido o está mal escrito" && echo "No hay escritorio elegido" >> $LOG
+				sleep 3s
+				echo "Las opciones posibles son: XFCE, GNOME, PLASMA, CINNAMON, MATE"
+				sleep 6s
+	}
+
 	while true; do
 		clear
-
-		echo "Instalando reflector y actualizando la mirrorlist del live para usar lor mirrors más rápidos"
-		pacman -Syy reflector --noconfirm && echo "reflector instalado correctamente" >> $LOG || echo "reflector no instalado" >> $LOG
-		reflector --latest 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist && echo "Mirror más rápido elegido" >> $LOG
-
 		echo "Se ha terminado de instalar, ¿desea apagar el equipo?"
 		echo ""
 
 		read input
 
 		case $input in
-			s|S) poweroff; break;;
+			y|Y) poweroff; break;;
 			n|N) exit 0;;
-			*) echo "Opción inválida [s/n]";;
+			*) echo "Opción inválida [y/n]";;
 		esac
 	done
 }
